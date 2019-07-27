e6247e2b23fb0d0ebd3cd01c8a65e83a8d9f7736
jhunlen <47968520+jhunlen@users.noreply.github.com>
2019-07-27 02:04:52 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+
+// $oneDime = array (
+// 	"Mango",
+// 	"Apple",
+// 	"Grapes"
+
+// );
+
+// var_dump($oneDime);
+// $twoDime = array (
+// 	array("apple", "mango"),
+// 	array("white", "blue")
+
+
+// );
+
+// echo "<br>";
+
+// print_r($twoDime);
+
+// echo "<br>";
+// echo $twoDime[0][1];
+// echo "<br>";
+// $keyValue = array(
+// 	"firstName" => "jhunlen",
+// 	"lastName" => "ajero"
+
+
+
+
+// );
+// echo "</br>";
+// print_r($keyValue);
+// echo $keyValue["firstName"];
+
+$firstname = "jhunlen"
+$lastname = "ajero";
+
+echo "$firstname lesano $lastname";
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ index4.php
@@ -0,0 +1,38 @@
+<?php 
+$firstname = "jhunlen";
+$lastname = "ajero";
+
+echo "$firstname lesano. $lastname";
+echo "<br>";
+
+$x = 5;
+$y = 10;
+
+
+echo $x + $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x - $y;
+
+
+$b = 5;
+$t = 6;
+$u = 7;
+$i = 8;
+$o = 9;
+$sum = $b + $t + $u +$i + $o;
+echo "$sum";
+
+
+
+
+
+
+
+
+
+
+ ?>
